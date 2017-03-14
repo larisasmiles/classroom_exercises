a138dc6aac6369712ceda8517577a4f5f86c757a
Larisasmiles <larisasmiles@gmail.com>
2017-03-14 12:54:37 -0600

    nini

---
+++ 1module/classroom_exercises/flow_control.rb
@@ -0,0 +1,17 @@
+# require "byebug"
+
+# 5.times do |i|
+#     i.even?
+#     puts "line is even"
+#     i.odd?
+#     puts "line is odd"
+# end
+
+# 5.times do |i|
+#   puts "This is my output line #{i+1}"
+   
+# end
+
+while i = 
+    puts "This is my output line #{i+1}"
+end
