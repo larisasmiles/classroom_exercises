72e4fed23645885647c15002759f5d2b3a8920cc
Larisasmiles <larisasmiles@gmail.com>
2017-03-14 12:47:16 -0600

    this is my first commit

---
+++ 1module/homework/pseudocode.rb
@@ -0,0 +1,19 @@
+# If input is nothing 
+# respond with Hello?!
+# If input is Hello? 
+# respond with "Hello, how can I help you?"
+# if input is in lowercase .
+# respond with "I AM HAVING A HARD TIME HEARING YOU."
+# if input is all uppercase letters 
+# respond with "NO, THIS IS NOT A PET STORE"
+# if input is goodbye for the 1st time 
+# respond with, "ANYTHING ELSE I CAN HELP WITH?"
+# if input is goodbye for the 2nd time 
+# respond with "THANK YOU FOR CALLING" 
+# and the program exits
+
+1.) when the "rules" wont need to be used 
+2.) as an automated site for campers at a specified camp site
+3.) if the customer needs to be directed to a person
+
+
