2a11592416c1ed27480dba579efaa856c9b3dfe3
Larisasmiles <larisasmiles@gmail.com>
2017-03-14 22:32:54 -0600

    threeloops

---
+++ 1module/classroom_exercises/rando_guesser.rb
@@ -0,0 +1,18 @@
+random_number = rand(0..10)
+secret_number = 8
+puts "Pick any number between 0 and 10:"
+guess = gets.chomp
+
+# until guess == random_number
+# 	puts "Guess is #{guess}" "\n Guess again!"
+# 	guess = gets.chomp.to_i
+    
+#     guess = random_number
+#     puts "You Win!"
+while secret_number != 8 
+    puts "Guess is #{guess}" "\n Guess again!"
+
+end
+  
+
+      
---
+++ 1module/classroom_exercises/three_loops.rb
@@ -0,0 +1,15 @@
+5.times do |i|
+    puts "This is my output line #{i+1}"
+end
+
+x = 1
+while x <= 5
+   puts "This is my output line #{x}"
+   x += 1
+end
+
+x = 1
+until x >=6
+    puts "This is my output line #{x}"
+    x += 1
+end
